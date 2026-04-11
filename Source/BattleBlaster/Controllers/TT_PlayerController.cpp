bb68d2ae6bc570f59c0415ebde77dd930238c9af
Nishant Verma <vnishant148@gmail.com>
2026-04-11 13:55:50 -0600

    Added Controller Base

--- Source/BattleBlaster/Controllers/TT_PlayerController.cpp
+++ Source/BattleBlaster/Controllers/TT_PlayerController.cpp
@@ -2,6 +2,54 @@
 
 
 #include "TT_PlayerController.h"
-#include "InputMappingContext.h"
-#include "EnhancedInputSubsystems.h"
 
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "GameFramework/Pawn.h"
+#include "InputActionValue.h"
+#include "InputMappingContext.h"
+
+void ATT_PlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto* USubsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+
+	USubsystem->AddMappingContext(MappingContext, 0);
+}
+
+void ATT_PlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	if (UEnhancedInputComponent* EIC = Cast<UEnhancedInputComponent>(InputComponent))
+	{
+		EIC->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ATT_PlayerController::HandleMove);
+	}
+}
+
+void ATT_PlayerController::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	if (ULocalPlayer* LocalPlayer = GetLocalPlayer())
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem =
+			ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(LocalPlayer))
+		{
+			if (MappingContext)
+			{
+				Subsystem->RemoveMappingContext(MappingContext);
+			}
+		}
+	}
+
+	Super::EndPlay(EndPlayReason);
+}
+
+void ATT_PlayerController::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);	
+}
+
+void ATT_PlayerController::HandleMove(const FInputActionValue& ActionValue)
+{
+}
--- Source/BattleBlaster/Controllers/TT_PlayerController.h
+++ Source/BattleBlaster/Controllers/TT_PlayerController.h
@@ -11,10 +11,27 @@
  */
 class UEnhancedInputComponent;
 class UInputMappingContext;
+class UEnhancedInputSubsystems;
+class UInputAction;
+struct FInputActionValue;
 
 UCLASS()
 class BATTLEBLASTER_API ATT_PlayerController : public APlayerController
 {
 	GENERATED_BODY()
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void SetupInputComponent() override;
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+	virtual void Tick(float DeltaSeconds) override;
+
+	void HandleMove(const FInputActionValue& ActionValue);
+
+	UPROPERTY(EditDefaultsOnly)
+	TObjectPtr<UInputMappingContext> MappingContext;
+
+	UPROPERTY(EditDefaultsOnly)
+	TObjectPtr<UInputAction> MoveAction;
 	
 };
